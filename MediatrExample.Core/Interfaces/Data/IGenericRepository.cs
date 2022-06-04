ff5324b0160886aa24fa789357749661353f0cc5
Hasan Erdal <hasaerda@hotmail.com>
2022-06-04 22:48:58 +0300

    implement generic respository pattern

---
+++ MediatrExample.Core/Interfaces/Data/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using MediatrExample.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrExample.Core.Interfaces.Data
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        Task<TEntity> GetByIdAsync(object id);
+        TEntity GetById(object id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> GetAll();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> JoinWhere(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> InsertAsyncReturn(TEntity entity);
+        Task InsertAsync(TEntity entity);
+        void Insert(TEntity entity);
+        TEntity InsertReturn(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object id);
+        TEntity Update(TEntity entity);
+    }
+}
---
+++ MediatrExample.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,126 @@
+﻿using MediatrExample.Core.Interfaces.Data;
+using MediatrExample.Data.Context;
+using MediatrExample.Shared.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrExample.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(object id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            var entities = _dbSet.ToList();
+            return entities;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            var entities = await _dbSet.ToListAsync();
+            return entities;
+        }
+
+        public TEntity GetById(object id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> InsertAsyncReturn(TEntity entity)
+        {
+            var data = await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return data.Entity;
+        }
+
+        public TEntity InsertReturn(TEntity entity)
+        {
+            var data = _dbSet.Add(entity);
+            _context.SaveChanges();
+            return data.Entity;
+        }
+
+        public IQueryable<TEntity> JoinWhere(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            query = query.Where(predicate);
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return query;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            var data = _dbSet.Update(entity);
+            _context.SaveChanges();
+            return data.Entity;
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}

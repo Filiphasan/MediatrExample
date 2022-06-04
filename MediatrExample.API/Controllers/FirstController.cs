b1f3ad657baa2d3f1a82b19516d6d1b4861e05ef
Hasan Erdal <hasaerda@hotmail.com>
2022-06-04 19:32:20 +0300

    create first controller

---
+++ MediatrExample.API/Controllers/FirstController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediatrExample.API.Controller
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        public FirstController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFirst()
+        {
+            return Ok("Server Working..");
+        }
+    }
+}

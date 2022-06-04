49ccb88a8671428a7119cb426ec1bd19cdbbe233
Hasan Erdal <hasaerda@hotmail.com>
2022-06-04 21:02:28 +0300

    .

--- MediatrExample.Shared/CustomMethod/MyCustomMethods.cs
+++ MediatrExample.Shared/CustomMethod/MyCustomMethods.cs
@@ -11,5 +11,17 @@
         {
             return dateTime.ToString("yyyy.MM.dd HH:mm");
         }
+
+        /// <summary>
+        /// Convert Datetime To String. Result Example: 2022.06.04 20:24 If null return string empty
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string GetDateTimeToMyString(this DateTime? dateTime)
+        {
+            string result = string.Empty;
+            if (dateTime.HasValue) result = dateTime.Value.ToString("yyyy.MM.dd HH:mm");
+            return result;
+        }
     }
 }

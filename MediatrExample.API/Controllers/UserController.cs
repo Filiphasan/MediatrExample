e729f34b96aecd8ff9f37083ca4d10bf7a84dda3
Hasan Erdal <hasaerda@hotmail.com>
2022-06-04 22:36:02 +0300

    create user controller

---
+++ MediatrExample.API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediatrExample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public async Task<IActionResult> GetUserList()
+        {
+            return Ok();
+        }
+    }
+}

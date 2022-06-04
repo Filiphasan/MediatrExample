6761d0ffaad19a477a52f64b87ab98317cdf7ff1
Hasan Erdal <hasaerda@hotmail.com>
2022-06-04 20:19:32 +0300

    edit dll referende and start entity coding

---
+++ MediatrExample.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using MediatrExample.Shared.Data;
+
+namespace MediatrExample.Core.Entities
+{
+    public class User : EntityBase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Gsm { get; set; }
+    }
+}
---
+++ MediatrExample.Shared/Data/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrExample.Shared.Data
+{
+    public class EntityBase : IEntity
+    {
+        public EntityBase()
+        {
+            CreatedTime = DateTime.Now;
+        }
+
+        public Nullable<DateTime> UpdatedTime { get; set; }
+        public Nullable<DateTime> CreatedTime { get; set; }
+    }
+}

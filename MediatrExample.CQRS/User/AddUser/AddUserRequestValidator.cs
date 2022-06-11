032181ca37e659954fc9674525a7200b141c4c2f
Hasan Erdal <hasaerda@hotmail.com>
2022-06-11 15:12:10 +0300

    refactor gsm validator

--- MediatrExample.CQRS/User/AddUser/AddUserRequestValidator.cs
+++ MediatrExample.CQRS/User/AddUser/AddUserRequestValidator.cs
@@ -13,15 +13,8 @@ namespace MediatrExample.CQRS.User.AddUser
             RuleFor(x => x.Gsm).NotNull().NotEmpty().Must(x =>
              {
                  string result = x.FormatGSMForTR();
-                 if (result == string.Empty)
-                 {
-                     return false;
-                 }
-                 else
-                 {
-                     x = result;
-                     return true;
-                 }
+                 x = result;
+                 return result == string.Empty;
              }).WithMessage("GSM Number has Wrong Format! Correct Format One Of '+905555555555,905555555555,05555555555,5555555555'");
             RuleFor(x => x.Password).NotNull().NotEmpty().Matches(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,16}$")
                 .WithMessage("Password must contain one uppercase, one lowercase, one number. Password length must beetween 6 and 16.");

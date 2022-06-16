91850720ad64a4d6f36b506cac84c009b7788553
Hasan Erdal <hasaerda@hotmail.com>
2022-06-16 20:39:44 +0300

    remove unuse using

--- MediatrExample.Core/Interfaces/Data/IGenericRepository.cs
+++ MediatrExample.Core/Interfaces/Data/IGenericRepository.cs
@@ -1,10 +1,5 @@
 ﻿using MediatrExample.Shared.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MediatrExample.Core.Interfaces.Data
 {
--- MediatrExample.Data/Repositories/GenericRepository.cs
+++ MediatrExample.Data/Repositories/GenericRepository.cs
@@ -2,12 +2,7 @@
 using MediatrExample.Data.Context;
 using MediatrExample.Shared.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MediatrExample.Data.Repositories
 {
